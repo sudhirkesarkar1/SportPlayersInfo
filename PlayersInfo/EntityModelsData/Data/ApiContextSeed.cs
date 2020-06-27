9478e7efd167f520080515f88829577ff0314844
sudhirkesarkar1 <sudhirkesarkar1@gmail.com>
2020-06-27 15:35:39 +0530

    Seeding Dummy data for initial EF core DB Migration

---
+++ PlayersInfo/EntityModelsData/Data/ApiContextSeed.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using PlayersInfo.EntityModelsData.Models.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace PlayersInfo.EntityModelsData.Data
+{
+    public class ApiContextSeed
+    {
+        public static async Task SeedAsync(ApiDbContext context, ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                if (!context.Countries.Any())
+                {
+                    var countriesData =
+                        File.ReadAllText("../Infrastructure/Data/SeedData/Countries.json");
+
+                    var countries = JsonSerializer.Deserialize<List<Country>>(countriesData);
+
+                    foreach (var country in countries)
+                    {
+                        context.Countries.Add(country);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.Games.Any())
+                {
+                    var gamesData =
+                        File.ReadAllText("../Infrastructure/Data/SeedData/Games.json");
+
+                    var games = JsonSerializer.Deserialize<List<Game>>(gamesData);
+
+                    foreach (var game in games)
+                    {
+                        context.Games.Add(game);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.Players.Any())
+                {
+                    var playersData =
+                        File.ReadAllText("../Infrastructure/Data/SeedData/Players.json");
+
+                    var players = JsonSerializer.Deserialize<List<Player>>(playersData);
+
+                    foreach (var player in players)
+                    {
+                        context.Players.Add(player);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<ApiDbContext>();
+                logger.LogError(ex.Message);
+            }
+        }
+    }
+}
